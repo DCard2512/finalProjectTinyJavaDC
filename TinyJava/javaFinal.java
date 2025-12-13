356a1c7eb4e8d71060841ee78d2ac027e38fb732
DCard2512 <diegocardenas.2512@gmail.com>
2025-12-12 22:39:10 -0800

    finalproject set up

---
+++ TinyJava/javaFinal.java
@@ -0,0 +1,7 @@
+package TinyJava;
+
+public class javaFinal {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ TinyJava/tokens.java
@@ -0,0 +1,5 @@
+package TinyJava;
+
+public class tokens {
+    
+}
